00842363f376d9d68c24d983cd0bc33248d6d885
yogymax@gmail.com <yogymax@gmail.com>
2018-06-23 12:59:40 +0530

    Conflict mode changes

--- src/main/java/com/scp/demo/ExcelReadWriteBasics.java
+++ src/main/java/com/scp/demo/ExcelReadWriteBasics.java
@@ -14,6 +14,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * 
  * This is for excel read write demo
  *
+ *This is demo
  */
 		public class ExcelReadWriteBasics {
 				public static void main(String[] args) throws IOException {
